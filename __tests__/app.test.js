f2b0dac4fafb6e043901de40299a1767834245ec
Moctor42 <moctor42@Ubuntu.myguest.virtualbox.org>
2024-02-19 11:39:45 +0000

    completed task 2

---
+++ __tests__/app.test.js
@@ -0,0 +1,30 @@
+const testData = require('../db/data/test-data/index')
+const db = require('../db/connection')
+const app = require('../app/app')
+const request = require('supertest')
+const seed = require('../db/seeds/seed')
+
+
+beforeEach(()=>seed(testData))
+afterAll(()=>db.end)
+
+describe('GET /api/topics', () => {
+    it('should respond with 200 status', () => {
+        return request(app)
+        .get('/api/topics')
+        .expect(200)
+    });
+    it('should respond with an array of topic objects, with properties "slug" and "description"', () => {
+        return request(app)
+        .get('/api/topics')
+        .expect(200)
+        .then((response)=>{
+            const topics = response.body.rows
+            expect(topics).toHaveLength(3)
+            topics.forEach((topic)=> {
+                expect(topic).toHaveProperty('slug')
+                expect(topic).toHaveProperty('description');
+            });            
+        })
+    });
+});
---
+++ app/controllers/app.controllers.js
@@ -0,0 +1,8 @@
+const { fetchTopics } = require('../models/app.models')
+
+exports.getTopics = (request, response)=>{
+    fetchTopics()
+    .then((result)=>{
+        response.status(200).send(result)
+    })
+}
