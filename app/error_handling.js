d48395df8eaef6943473bcb82b0c48072f25e254
Moctor42 <moctor42@Ubuntu.myguest.virtualbox.org>
2024-02-21 14:57:19 +0000

    completed task 8-patch-article

--- app/error_handling.js
+++ app/error_handling.js
@@ -16,11 +16,10 @@ exports.error400 = (error, request, response, next)=>{
 
 exports.psqlError = (error, request, response, next)=>{
     if (error.code === '22P02') {
-        response.status(400).send({msg: 'bad request'})
+        response.status(400).send({msg: 'bad request', error: error})
     } else if (error.code === '23503'){
         const regex = /\w*(?=s")/g
         let msgString = error.detail.match(regex)[0] + ' not found'
-        console.log(error.detail)
         response.status(404).send({msg: msgString})
     } else if (error.code === '23502'){
         response.status(400).send({msg: 'missing property'})
