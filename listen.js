ec3b5ccea6ad917888fe91c52d0d444c31c84798
Moctor42 <moctor42@Ubuntu.myguest.virtualbox.org>
2024-02-23 11:35:52 +0000

    double checked dependencies

--- listen.js
+++ listen.js
@@ -1,4 +1,4 @@
-const app = require('./app/app')
+const app = require('./app/app.js')
 const { PORT = 9000 } = process.env
 
-app.listen(PORT ,() => console.log(`listening on ${PORT}...`))
+app.listen(PORT , () => console.log(`listening on ${PORT}...`))
